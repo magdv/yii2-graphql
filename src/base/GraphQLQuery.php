e5da4a222ae9fdd7159f6f5cebf9d565083775f6
Emmanuel Nkrumah-Sarpong <emmanuel.sarpong@platoanalytics.com>
2021-09-09 11:09:25 +0000

    create ModelQuery to maintain GraphQLQuery api

--- src/base/GraphQLQuery.php
+++ src/base/GraphQLQuery.php
@@ -7,23 +7,5 @@ use yii\graphql\GraphQL;
 
 class GraphQLQuery extends GraphQLField
 {
-    public $type;
-    public $model;
-
-    public function type()
-    {
-        return Type::listOf(GraphQL::type($this->type));
-    }
-
-    public function args()
-    {
-        return [
-            'id' => Type::id(),
-        ];
-    }
-
-    public function resolve($value, $args, $context, ResolveInfo $info)
-    {
-        return $this->model::find()->where($args)->all();
-    }
+    
 }
